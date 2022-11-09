a101c8b85f7a4e7fa2e6e080c774d780e836bb45
henriquesdias <henrique.dias1915@gmail.com>
2022-11-09 09:14:24 -0300

    build: joi and cors were installed

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import express from "express";
+var server = express();
+server.listen(4000, function () { return console.log("Listening on port 4000"); });
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const server = express();
+
+server.listen(4000, () => console.log("Listening on port 4000"));
